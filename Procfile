ce055e60a5cb509602512395f3bfdb020d986ec5
Madhulika <hmadhulika@gmail.com>
2019-04-12 20:53:50 +0530

    Changes to run in python3

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: python manage.py runserver $PORT
+web: python3 manage.py runserver $PORT
--- fibonacciCallHub/views.py
+++ fibonacciCallHub/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.template import Template 
 from django.shortcuts import render, redirect
+import time
 
 def homePage(request):
 	# return HttpResponse("Hello from Home Page")
@@ -8,6 +9,23 @@ def homePage(request):
 
 def result(request):
 	if request.method == 'POST':
+		start_time = time.time()
+
 		user_entered = request.POST.get('fnumber')
 		print(user_entered)
-		return  render(request,'fibonacciCallHub/result.html', {'number':user_entered})
+		if user_entered == '':
+			return  render(request,'fibonacciCallHub/homepage.html', {'number':user_entered, 'message': "Please enter the number"})
+		else:
+			fibNum = fibonacci(int(user_entered))
+			
+			time_diff = time.time() - start_time
+
+			return  render(request,'fibonacciCallHub/homepage.html', {'number':user_entered, 'fibNum':fibNum, 'timeDiff':float(round(time_diff,5))})
+
+def fibonacci(n):
+    if n==0: 
+        return 0
+    elif n==1: 
+        return 1
+    else: 
+        return fibonacci(n-1)+fibonacci(n-2)

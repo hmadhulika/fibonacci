7a6b8fb06d9b59b4431026c7a0b28816ee06aef2
Madhulika <hmadhulika@gmail.com>
2019-04-13 12:56:46 +0530

    optimized code

--- fibonacciCallHub/views.py
+++ fibonacciCallHub/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.template import Template 
 from django.shortcuts import render, redirect
 import time
+import math
 
 def homePage(request):
 	# return HttpResponse("Hello from Home Page")
@@ -9,23 +10,41 @@ def homePage(request):
 
 def result(request):
 	if request.method == 'POST':
-		start_time = time.time()
-
 		user_entered = request.POST.get('fnumber')
-		print(user_entered)
+		
 		if user_entered == '':
 			return  render(request,'fibonacciCallHub/homepage.html', {'number':user_entered, 'message': "Please enter the number"})
 		else:
+			start_time = time.time()
+
 			fibNum = fibonacci(int(user_entered))
 			
 			time_diff = time.time() - start_time
 
-			return  render(request,'fibonacciCallHub/homepage.html', {'number':user_entered, 'fibNum':fibNum, 'timeDiff':float(round(time_diff,5))})
+			# start_time1 = time.time()
+			
+			# fibNum1 = fibonacci1(int(user_entered))
+			
+			# time_diff1 = time.time() - start_time
 
+			return  render(request,'fibonacciCallHub/homepage.html', {'number':user_entered, 'fibNum':fibNum, 'timeDiff':float(time_diff)})
+
+# def fibonacci1(n):
+#     if n==0: 
+#         return 0
+#     elif n==1: 
+#         return 1
+#     else:
+#         return fibonacci(n-1)+fibonacci(n-2)
+
+
+## Optimized code
+# Using the formula Fn = {[(√5 + 1)/2] ^ n} / √5 
+# Time Complexity = O(1)
 def fibonacci(n):
-    if n==0: 
-        return 0
-    elif n==1: 
-        return 1
-    else: 
-        return fibonacci(n-1)+fibonacci(n-2)
+	
+	x = ((math.sqrt(5)+1)/2)
+	
+	y = (x ** n)/math.sqrt(5)
+
+	return int(y)
